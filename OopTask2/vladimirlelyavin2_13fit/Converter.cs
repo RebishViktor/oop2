98d736e71c64383ebaea95a9dc88d50dc9bdc98f
vlelyavin <vlelyavin@gmail.com>
2022-09-26 01:22:09 +0300

    final program

---
+++ OopTask2/vladimirlelyavin2_13fit/Converter.cs
@@ -0,0 +1,35 @@
+﻿
+namespace OopTask2.vladimirlelyavin2_13fit
+{
+    public class Converter {
+
+        private double usd;
+        private double eur;
+        private double rub;
+
+        public Converter(double usd, double eur, double rub)
+        {
+            this.usd = usd;
+            this.eur = eur;
+            this.rub = rub;
+        }
+
+        public int convertUAHToUSD(double amount)
+        {
+            return (int)Math.Round((amount / usd)); 
+        }
+
+        public int convertUSDToUAH(double amount)
+        {
+            return (int)Math.Round(amount * usd);
+        }
+
+        public int convertEURToUAH(double amount) {
+            return (int)Math.Round(amount * eur);  
+        }
+        public int convertRUBToUAH(double amount)
+        {
+            return (int)Math.Round(amount * rub); 
+        }
+    }
+}
